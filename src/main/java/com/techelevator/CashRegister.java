f02db81fc6714fdeb723d6639793011ea326fc95
Lindsay Leigh <LindsayJoLeigh@gmail.com>
2021-02-05 22:29:33 -0500

    Inventory Class add, Map created, updates to Cash Register class

--- src/main/java/com/techelevator/CashRegister.java
+++ src/main/java/com/techelevator/CashRegister.java
@@ -1,6 +1,7 @@
 package com.techelevator;
 
 import java.math.BigDecimal;
+import java.util.Scanner;
 
 public class CashRegister {
 	
@@ -8,14 +9,15 @@ public class CashRegister {
 	private BigDecimal dime = new BigDecimal(".10");
 	private BigDecimal quarter = new BigDecimal(".25");
 	private BigDecimal oneDollar = new BigDecimal("1.00");
+	private BigDecimal twoDollar = new BigDecimal("2.00");
 	private BigDecimal fiveDollar = new BigDecimal("5.00");
 	private BigDecimal tenDollar = new BigDecimal("10.00");
-	private BigDecimal twentyDollar = new BigDecimal("20.00");
-	private BigDecimal customerBalance;
-	private String customerCoinChange;
+	private BigDecimal customerBalance = new BigDecimal("0");
+	private String customerCurrencyChange;
+	private String customerChangeTotal;
 	
-	public CashRegister(BigDecimal customerBalance) {
-		this.customerBalance = customerBalance;
+	public CashRegister() {
+
 	}
 	
 	public BigDecimal getNickel() {
@@ -49,58 +51,97 @@ public class CashRegister {
 	public BigDecimal getCustomerBalance() {
 		return customerBalance;
 	}
+	
+	public BigDecimal setCustomerBalance() {
+		
+		Scanner userInput = new Scanner(System.in);
+		System.out.println("Please enter dollar bills");
+		System.out.println("(Example: 1 2 5 10" );
+		String dollarInput = userInput.nextLine();
+		String[] dollarInputArray = dollarInput.split(",");
+		
+		for (int i = 0; i < dollarInputArray.length; i++) {
+		
+			if (dollarInput.equals("1")) {
+			customerBalance = customerBalance.add(oneDollar);
+			} else if (dollarInput.equals("2")) {
+			customerBalance = customerBalance.add(twoDollar);
+			} else if (dollarInput.equals("5")) {
+			customerBalance = customerBalance.add(fiveDollar);
+			} else if (dollarInput.equals("10")) {
+				customerBalance = customerBalance.add(tenDollar);
+				}
+		
+			} return customerBalance;
+	}
 
-	public void getCustomerCoinChange() {
+	
+	public void getCustomerCurrencyChange() { //DONE
 		int dollarCount = 0;
 		int quarterCount = 0;
 		int dimeCount = 0;
 		int nickelCount = 0;
 		String thankYou = "Thank you for your business.";
 		
-		//2 methods? one for bills , one for coins?
 		
-		if (customerBalance.compareTo(BigDecimal.valueOf(1)) < 0)  {
-			
-		//COIN CHANGE
-		//check for quarters
-		BigDecimal quarters = customerBalance.divide(quarter);
-		quarterCount = quarters.intValue();
-		customerBalance = customerBalance.subtract(quarter.multiply(BigDecimal.valueOf(quarterCount)));
+		//dollar change
+		if (customerBalance.compareTo(BigDecimal.valueOf(0)) > 0) {
+		BigDecimal dollars = customerBalance.divide(oneDollar);
+		dollarCount = dollars.intValue();
+		customerBalance = customerBalance.subtract(oneDollar.multiply(BigDecimal.valueOf(dollarCount)));
 			if (customerBalance.compareTo(BigDecimal.valueOf(0)) == 0) {
-				String toQuarterChange = "Your change is " + quarterCount + " quarter(s)"; 
-				System.out.println(thankYou + toQuarterChange);
-			} 
+				String toDollarChange = "Your change is " + dollarCount + " dollar(s).";
+				System.out.println(thankYou + toDollarChange);
+			} else if (customerBalance.compareTo(BigDecimal.valueOf(1)) < 0)  {	
+				//COIN CHANGE
+				//check for quarters
+				BigDecimal quarters = customerBalance.divide(quarter);
+				quarterCount = quarters.intValue();
+				customerBalance = customerBalance.subtract(quarter.multiply(BigDecimal.valueOf(quarterCount)));
+					if (customerBalance.compareTo(BigDecimal.valueOf(0)) == 0) {
+					String toQuarterChange = "Your change is " + dollarCount + " dollar(s), " + quarterCount + " quarter(s)"; 
+					System.out.println(thankYou + toQuarterChange);
+					} 
 		
-			//check for dimes
-			BigDecimal dimes = customerBalance.divide(dime);
-			dimeCount = dimes.intValue();
-			customerBalance = customerBalance.subtract(dime.multiply(BigDecimal.valueOf(dimeCount)));
-				if (customerBalance.compareTo(BigDecimal.valueOf(0)) == 0) {
-				String toDimeChange = "Your change is " + quarterCount + " quarter(s), and " + dimeCount + " dime(s)";
-				System.out.println(thankYou + toDimeChange);
-				} else {
+					//check for dimes
+					BigDecimal dimes = customerBalance.divide(dime);
+					dimeCount = dimes.intValue();
+					customerBalance = customerBalance.subtract(dime.multiply(BigDecimal.valueOf(dimeCount)));
+						if (customerBalance.compareTo(BigDecimal.valueOf(0)) == 0) {
+						String toDimeChange = "Your change is " + dollarCount + " dollar(s), " + quarterCount + " quarter(s), and " + dimeCount + " dime(s)";
+						System.out.println(thankYou + toDimeChange);
+							} else {
 		
-				//check for nickels
-				BigDecimal nickels = customerBalance.divide(nickel);
-				nickelCount = nickels.intValue();
-				customerBalance = customerBalance.subtract(nickel.multiply(BigDecimal.valueOf(nickelCount)));
-				String toNickelChange = "Your change is " + quarterCount + " quarter(s), " + dimeCount + " dime(s), and " + nickelCount + " nickel(s). ";
-				System.out.println(thankYou + toNickelChange);
-				}
-		}		
-		System.out.println(thankYou + "You have no chenge.");;
+								//check for nickels
+								BigDecimal nickels = customerBalance.divide(nickel);
+								nickelCount = nickels.intValue();
+								customerBalance = customerBalance.subtract(nickel.multiply(BigDecimal.valueOf(nickelCount)));
+								String toNickelChange = "Your change is " + dollarCount + " dollar(s), " + quarterCount + " quarter(s), " + dimeCount + " dime(s), and " + nickelCount + " nickel(s). ";
+								System.out.println(thankYou + toNickelChange);
+							}
+					}		
+		
+		} else {
+			System.out.println(thankYou + "There is no change to return.");
+		}
 	}
 
 
 	public String notEnoughMoney(boolean isEnough) {
-		
+		if (customerBalance.compareTo(Product.getPrice()) > 0) {
+			
+		}
 			
 		
 		return "You do not have enough money for item selected.";
 	}
 	
 	
-	
+	public BigDecimal resetBalanceToZero() {
+		customerBalance = 0;
+		return customerBalance;
+		
+	}
 	
 	
 	
---
+++ src/main/java/com/techelevator/Inventory.java
@@ -0,0 +1,52 @@
+package com.techelevator;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Inventory {
+
+	
+	public Map<String, String> productsByCode = new HashMap<>();
+	
+	
+	public Map<String, String> setProductList() {
+		File inventoryFile = new File("vendingmachine.csv");
+			
+		try(Scanner inventoryScanner = new Scanner(inventoryFile)) {
+				while(inventoryScanner.hasNextLine()) {
+					String line = inventoryScanner.nextLine(); //A1|Potato Crisps|3.05|Chip
+					String productParts = line.replaceAll("\\|", " "); //A1, Potato, 3.05 , Chip
+					String productKey = productParts.substring(0, 3);
+					String productString = productParts.substring(3);
+					productsByCode.put(productKey, productString);              
+				}
+				return productsByCode;
+			}
+	}	
+	
+	public String getProductString(String code) {
+		return productsByCode.get(code);
+	}
+	
+//	A1|Potato Crisps|3.05|Chip
+//	A2|Stackers|1.45|Chip
+//	A3|Grain Waves|2.75|Chip
+//	A4|Cloud Popcorn|3.65|Chip
+//	B1|Moonpie|1.80|Candy
+//	B2|Cowtales|1.50|Candy
+//	B3|Wonka Bar|1.50|Candy
+//	B4|Crunchie|1.75|Candy
+//	C1|Cola|1.25|Drink
+//	C2|Dr. Salt|1.50|Drink
+//	C3|Mountain Melter|1.50|Drink
+//	C4|Heavy|1.50|Drink
+//	D1|U-Chews|0.85|Gum
+//	D2|Little League Chew|0.95|Gum
+//	D3|Chiclets|0.75|Gum
+//	D4|Triplemint|0.75|Gum
+		
+
+}
