10e0d9ee478582c8de762f90c5849d4958a28398
Lindsay Leigh <LindsayJoLeigh@gmail.com>
2021-02-07 17:26:37 -0500

    CashRegiterTest class started

---
+++ src/test/java/com/techelevator/CashRegisterTest.java
@@ -0,0 +1,20 @@
+package com.techelevator;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CashRegisterTest {
+
+	@Test
+	public void getCustomerBalanceReturnsBalance() {
+		//Arrange
+		CashRegister success = new CashRegister();
+		
+		//Act
+		
+		//Assert
+
+	}
+	
+	
+}
